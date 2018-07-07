7ee5b33e1620b3ada934d4e7c9f33dde434a839f
Ash <amurphy9956@live.com>
2018-07-07 18:10:05 +0700

    If applied, this commit will... Add tox to the project
    Why is this change needed?
    Moving towards automated testing/builds.
    
    How does it address the issue?
    Tox is great for adding automated tests/build functions
    and allows us to run these commands against multiple interpreters.

--- folium_map/test/test_folium_map.py
+++ folium_map/test/test_folium_map.py
@@ -3,11 +3,11 @@ import folium_map
 
 class TestUrlProducer:
 
-    def test_return_image_one():
+    def test_return_image_one(self):
         assert folium_map.url_producer(500) == 'icons/vol1.png'
 
-    def test_return_image_two():
+    def test_return_image_two(self):
         assert folium_map.url_producer(1500) == 'icons/vol2.png'
 
-    def test_return_image_three():
+    def test_return_image_three(self):
         assert folium_map.url_producer(5000) == 'icons/vol3.png'
